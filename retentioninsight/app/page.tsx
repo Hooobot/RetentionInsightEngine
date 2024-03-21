6c788f3484e782bbaae5606850ca5611162ca08b
Hooobot <hoobot0@gmail.com>
2024-03-20 16:35:54 -1000

    Front page theming

--- retentioninsight/app/page.tsx
+++ retentioninsight/app/page.tsx
@@ -1,12 +1,38 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// pages/index.tsx
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "./styles/Home.module.css";
+import logo from "./RIE_Logo.webp";
 
-export default function Home() {
+const Home: NextPage = () => {
   return (
-    <main className={styles.main}>
-      <h1>Retention Insight Engine</h1>
+    <div className={styles.container}>
+      <Head>
+        <title>Retention Insight Engine</title>
+        <meta name="description" content="Your go-to solution for insights" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-      <button className={styles.button}>Upload audio/video file</button>
-    </main>
+      <main className={styles.main}>
+        <img src={logo.src} alt="Logo" className={styles.logo} />
+        <h1 className={styles.title}>Retention Insight Engine</h1>
+
+        <div className={styles.uploadContainer}>
+          <label htmlFor="upload-button" className={styles.uploadButton}>
+            <button id="upload-button">Upload Files</button>
+          </label>
+          <input
+            type="file"
+            id="upload-button"
+            name="uploads"
+            accept=".mp3,.mp4"
+            multiple
+            style={{ display: "none" }}
+          />
+        </div>
+      </main>
+    </div>
   );
-}
+};
+
+export default Home;
