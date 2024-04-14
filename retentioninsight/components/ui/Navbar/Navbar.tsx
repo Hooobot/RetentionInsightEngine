6fea9b00441baec41683fbfee23b7213bb8c49a5
Hooobot <hoobot0@gmail.com>
2024-04-13 18:29:26 -1000

    Mobile navbar dropdown implementation

--- retentioninsight/components/ui/Navbar/Navbar.tsx
+++ retentioninsight/components/ui/Navbar/Navbar.tsx
@@ -1,16 +1,25 @@
+"use client";
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import RIE_Logo from "@/public/RIE_Logo2.png";
 
 const Navbar = () => {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+
   return (
     <nav className="navbar">
-      <div className="logo-container">
-        <Link href="/">
-          <Image src={RIE_Logo} alt="RIE Logo" width={70} height={63} />
-        </Link>
-      </div>
-      <div className="nav-links">
+      <Link href="/">
+        <Image src={RIE_Logo} alt="RIE Logo" width={70} height={63} />
+      </Link>
+      <button
+        className="mobile-nav-toggle"
+        aria-expanded={isNavExpanded}
+        onClick={() => setIsNavExpanded(!isNavExpanded)}
+      >
+        <div className="hamburger"></div>
+      </button>
+      <div className={`nav-links ${isNavExpanded ? "expanded" : ""}`}>
         <Link href="/">
           <span className="nav-link">Home</span>
         </Link>
