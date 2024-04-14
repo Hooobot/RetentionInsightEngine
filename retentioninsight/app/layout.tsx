7d747d39bf74ab99dd0e517d568526003bfbb16f
Hooobot <hoobot0@gmail.com>
2024-04-13 17:22:24 -1000

    Homepage and layout UI cleanliness

--- retentioninsight/app/layout.tsx
+++ retentioninsight/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Navbar from "@/components/ui/Navbar/Navbar";
+import Footer from "@/components/ui/Footer/Footer";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
--- retentioninsight/app/page.tsx
+++ retentioninsight/app/page.tsx
@@ -8,7 +8,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "./styles/Home.module.css";
-import logo from "./RIE_Logo.png";
+import logo from "../public/RIE_Logo.png";
 
 const Home: NextPage = () => {
   // State to hold uploaded files
